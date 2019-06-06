582f8151b8dd48309da4c1a0057b316d618d45ae
刘进行 <jackingstarliu@gmail.com>
2019-06-06 09:59:07 +0800

    feat(eslint-config-ts-lib): eslint支持immer

--- packages/eslint-config-ts-lib/index.js
+++ packages/eslint-config-ts-lib/index.js
@@ -51,6 +51,29 @@ const eslintConfig = {
       'error',
       { extensions: ['.js', '.jsx', '.tsx'] },
     ],
+    // disallow reassignment of function parameters
+    // disallow parameter object manipulation except for specific exclusions
+    // rule: https://eslint.org/docs/rules/no-param-reassign.html
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: [
+          'acc', // for reduce accumulators
+          'accumulator', // for reduce accumulators
+          'e', // for e.returnvalue
+          'ctx', // for Koa routing
+          'req', // for Express requests
+          'request', // for Express requests
+          'res', // for Express responses
+          'response', // for Express responses
+          '$scope', // for Angular 1 scopes
+          'staticContext', // for ReactRouter context
+          'draft', // for immer
+          'draftState', // for immer
+        ],
+      },
+    ],
   },
 };
 
