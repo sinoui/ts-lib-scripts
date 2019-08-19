78e127595330dc6f438976311cb4e7641ad14384
Jacking Liu <jackingstarliu@gmail.com>
2019-08-19 09:08:41 +0800

    fix(ts-lib-tools): 修复monorepo模式下，yarn lint找不到文件的缺陷

--- packages/ts-lib-tools/src/lint.ts
+++ packages/ts-lib-tools/src/lint.ts
@@ -1,12 +1,14 @@
 import execa from 'execa';
+import { isMonorepo } from 'ts-lib-scripts-utils';
 import { rootPath } from './config/paths';
 
 // eslint-disable-next-line import/prefer-default-export
 export async function lint() {
   const otherArgv = process.argv.slice(3);
+  const mono = isMonorepo();
   await execa(
     'eslint',
-    ['--color', '--ext', '.ts', '--ext', '.tsx', 'src/', ...otherArgv],
+    ['--color', '--ext', '.ts', '--ext', '.tsx', mono? 'packages/' : 'src/', ...otherArgv],
     {
       cwd: rootPath,
     },
