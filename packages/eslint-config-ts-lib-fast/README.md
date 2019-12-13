372336b197a7ddf54c86ba021892ac9ba248f5ce
刘进行 <jackingstarliu@gmail.com>
2019-12-13 17:57:34 +0800

    feat(eslint-config-ts-lib-fast): 用于代码提交前的检查的eslint配置库

---
+++ packages/eslint-config-ts-lib-fast/README.md
@@ -0,0 +1,3 @@
+# eslint-config-ts-lib-fast
+
+[ts-lib-scripts](https://github.com/sinoui/ts-lib-scripts) 创建的 ts 库项目使用的 ESLint 配置（快速版），用于代码提交前的检查。
---
+++ packages/eslint-config-ts-lib-fast/index.js
@@ -0,0 +1,9 @@
+const eslintConfig = {
+  extends: ['ts-lib'],
+  rules: {
+    'prettier/prettier': 'off',
+    'import/no-cycle': 'off',
+  },
+};
+
+module.exports = eslintConfig;
